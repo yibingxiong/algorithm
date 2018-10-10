de8a73afe57087cd38a8b56cb2570b312c2293c6
Bingxiong Yi <yibingxiong@58ganji.com>
2018-10-10 08:54:22 +0800

    归并排序,还得改改

---
+++ chapter2/2.4-merge.js
@@ -0,0 +1,37 @@
+let aux = [];
+function merge(arr, lo, mid, hi) {
+    let i = lo;
+    let j = mid + 1;
+    for (let k = lo; k <= hi; k++) {
+        aux[k] = arr[k];
+    }
+
+    for (let k = lo; k <= hi; k++) {
+        if(i > mid) {
+            arr[k] = aux[j++];
+        }else if(j > hi) {
+            arr[k] = aux[i++];
+        }else if(aux[j] < aux[i]) {
+            arr[k] = aux[j++];
+        }else {
+            arr[k] = aux[i++];
+        }
+    }
+}
+function sort(arr, lo, hi) {
+    if(hi <= lo) {
+        return;
+    }
+    let mid = Math.floor(lo + (hi - lo)/2);
+    sort(arr, lo, mid);
+    sort(arr, mid+1, hi);
+    merge(arr, lo, mid, hi);
+}
+function mergeSort(arr) {
+    sort(arr, 0, arr.length - 1);
+    return arr;
+}
+
+
+console.log(mergeSort([1,2,3,5,6,3,4]));
+// console.log(arr)
---
+++ chapter2/test.js
@@ -0,0 +1,7 @@
+function test(arr) {
+ arr[0] = 2;
+}
+
+let a = [0,3,1]
+test(a);
+console.log(a)
