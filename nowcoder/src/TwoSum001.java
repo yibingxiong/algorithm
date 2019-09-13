b2fe29ffe8cb299799b48d501ce32259a0197600
yibingxiong <yibingxiong@126.com>
2019-09-13 21:40:27 +0800

    leetcode-001-两数和问题

---
+++ nowcoder/src/TwoSum001.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+public class TwoSum001 {
+	public static int[] twoSum(int[] nums, int target) {
+        if (nums.length < 2) {
+            return null;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        int []res = new int[2];
+        
+        for (int i=0;i<nums.length;i++) {
+        	map.put(nums[i], i);
+        }
+        
+        for (int i = 0; i<nums.length; i++) {
+        	Integer k = map.get(target-nums[i]);
+        	if (k != null) {
+        		res[0] = i;
+        		res[1] = k;
+        		return res;
+        	}
+        }
+        return null;
+    }
+	
+	public static void main(String[] args) {
+		int [] a = {1,3,4,2,5,8,7};
+		int t = 9;
+		int []res = twoSum(a, t);
+		if (res!=null) {
+			System.out.print(res[0]+","+res[1]);
+		} else {
+			System.out.println("null");
+		}
+	}
+}
