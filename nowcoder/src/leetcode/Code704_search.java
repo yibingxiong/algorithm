d98854909757702be02fc61a2520877f3d97ff23
yibingxiong <yibingxiong@126.com>
2019-10-28 00:03:13 +0800

    leetcode

--- nowcoder/src/leetcode/Code704_search.java
+++ nowcoder/src/leetcode/Code704_search.java
@@ -3,12 +3,14 @@ package leetcode;
 public class Code704_search {
 
 	public static int search(int[] nums, int target) {
+		if (nums == null) {
+			return -1;
+		}
 		int left = 0;
-		int right = nums.length-1;
+		int right = nums.length - 1;
 
-		while (left < right) {
+		while (left <= right) {
 			int mid = left + (right - left) / 2;
-			System.out.println("mid="+mid+"nums[mid]="+nums[mid]);
 			if (nums[mid] == target) {
 				return mid;
 			} else if (nums[mid] > target) {
@@ -23,7 +25,7 @@ public class Code704_search {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = {-1,0,3,5,9,12};
+		int[] nums = { -1, 0, 3, 5, 9, 12 };
 		System.out.println(search(nums, 9));
 	}
 
