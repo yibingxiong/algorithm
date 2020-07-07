93959ef7093d23e2a2945167c6bbf82f4b5b424d
yibingxiong <yibingxiong@126.com>
2020-07-07 09:31:33 +0800

    restart

--- code/src/main/java/book/chapter5/section7/HashFamily.java
+++
@@ -1,9 +0,0 @@
-package book.chapter5.section7;
-
-public interface HashFamily<T> {
-    int hash(T x, int which);
-
-    int getNumberOfFunctions();
-
-    void generateNewFunctions();
-}
--- 历史代码封印/chapter2/test.js
+++
@@ -1,7 +0,0 @@
-function test(arr) {
- arr[0] = 2;
-}
-
-let a = [0,3,1]
-test(a);
-console.log(a)
