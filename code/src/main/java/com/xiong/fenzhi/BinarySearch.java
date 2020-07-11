087590adf94f60bdc8cf853e3cb5e55c7234e40e
yibingxiong <yibingxiong@126.com>
2020-07-11 09:31:51 +0800

    分治-二分查找非递归实现

--- code/src/main/java/com/xiong/fenzhi/BinarySearch.java
+++ code/src/main/java/com/xiong/fenzhi/BinarySearch.java
@@ -2,15 +2,6 @@ package com.xiong.fenzhi;
 
 public class BinarySearch {
     public static int search(int[] arr, int m, int n, int k) {
-        if (arr.length == 1) {
-            if (arr[0] == k) {
-                return 0;
-            } else {
-                return -1;
-            }
-        }
-
-
         if (m <= n) {
             int mid = (m + n) / 2;
             if (arr[mid] == k) {
@@ -25,14 +16,33 @@ public class BinarySearch {
         }
     }
 
+    public static int search2(int[] arr, int k) {
+        int m = 0;
+        int n = arr.length - 1;
+
+
+        while (m <= n) {
+            int mid = (n + m) / 2;
+            if (arr[mid] == k) {
+                return mid;
+            } else if (arr[mid] < k) {
+                m = mid + 1;
+            } else {
+                n = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 6, 8, 9, 10};
-
-        System.out.println(search(arr, 0, arr.length - 1, 6));
-        System.out.println(search(arr, 0, arr.length - 1, 11));
-        System.out.println(search(arr, 0, arr.length - 1, 7));
-        System.out.println(search(arr, 0, arr.length - 1, 1));
-        System.out.println(search(arr, 0, arr.length - 1, 10));
+        int[] arr2 = {1};
+//        System.out.println(search(arr, 0, arr.length - 1, 6));
+//        System.out.println(search(arr, 0, arr.length - 1, 11));
+//        System.out.println(search(arr, 0, arr.length - 1, 7));
+//        System.out.println(search(arr, 0, arr.length - 1, 1));
+//        System.out.println(search(arr, 0, arr.length - 1, 10));
+        System.out.println(search2(arr2, 1));
 
     }
 }
