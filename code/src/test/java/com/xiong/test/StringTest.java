7b74c13ceba4f40d5b49803383d6194cc9b245bc
yibingxiong <yibingxiong@126.com>
2020-07-28 23:30:18 +0800

    动态规划的基本思想

---
+++ code/src/test/java/com/xiong/test/StringTest.java
@@ -0,0 +1,15 @@
+package com.xiong.test;
+
+import org.junit.Test;
+
+public class StringTest {
+    @Test
+    public void test() {
+        String a = null;
+
+
+        String b = (String)null;
+
+        System.out.println(b);
+    }
+}
