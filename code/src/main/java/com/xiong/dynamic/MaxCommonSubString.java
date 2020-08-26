257d2fcbf8241997f109bae638416dc6101ad9f6
yibingxiong <yibingxiong@126.com>
2020-08-26 08:47:00 +0800

    最长公共子串问题

--- code/src/main/java/com/xiong/dynamic/MaxCommonSubString.java
+++ code/src/main/java/com/xiong/dynamic/MaxCommonSubString.java
@@ -36,9 +36,50 @@ public class MaxCommonSubString {
         System.out.println(new String(ca, k - max, max));
     }
 
+    public static void cal2(String a, String b) {
+        char[] charA = a.toCharArray();
+        char[] charB = b.toCharArray();
+        int lenA = charA.length;
+        int lenB = charB.length;
+        int[][] m = new int[lenA + 1][lenB + 1];
+
+        for (int i = 0; i <= lenA; i++) {
+            m[i][0] = 0;
+        }
+
+        for (int i = 0; i <= lenB; i++) {
+            m[0][i] = 0;
+        }
+
+        int max = 0;
+        int indexA = -1;
+        int indexB = -1;
+        for (int i = 1; i <= lenA; i++) {
+            for (int j = 1; j <= lenB; j++) {
+                if (charA[i - 1] == charB[j - 1]) {
+                    m[i][j] = m[i - 1][j - 1] + 1;
+                } else {
+                    m[i][j] = 0;
+                }
+                if (m[i][j] > max) {
+                    max = m[i][j];
+                    indexA = i;
+                    indexB = j;
+                }
+            }
+        }
+
+        System.out.println("长度" + max);
+
+        System.out.println("第一个字符串" + (indexA - max + 1) + "," + indexA);
+        System.out.println("第二个字符串" + (indexB - max + 1) + "," + indexB);
+    }
+
     public static void main(String[] args) {
         String a = "abcdef";
         String b = "badef";
         cal1(a, b);
+        System.out.println("--------2------------");
+        cal2(a, b);
     }
 }
