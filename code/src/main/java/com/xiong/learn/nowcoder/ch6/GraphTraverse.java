7da9f37e75c2519b91759d46119a0a8d75fe7370
yibingxiong <yibingxiong@126.com>
2020-09-26 17:57:55 +0800

    dfs

--- code/src/main/java/com/xiong/learn/nowcoder/ch6/GraphTraverse.java
+++ code/src/main/java/com/xiong/learn/nowcoder/ch6/GraphTraverse.java
@@ -1,9 +1,6 @@
 package com.xiong.learn.nowcoder.ch6;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class GraphTraverse {
     public static void traverseBFS(Node node) {
@@ -28,15 +25,43 @@ public class GraphTraverse {
         }
     }
 
+    public static void traverseDFS(Node node) {
+        if (node == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        stack.push(node);
+        HashSet<Node> visit = new HashSet<>();
+        System.out.println(node.val);
+        visit.add(node);
+        while (!stack.isEmpty()) {
+            Node pop = stack.pop();
+            ArrayList<Node> nexts = pop.nexts;
+            for (Node n : nexts) {
+                if (!visit.contains(n)) {
+                    visit.add(n);
+                    stack.push(pop);
+                    stack.push(n);
+                    System.out.println(n.val);
+                    break;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Node n1 = new Node(1);
         Node n2 = new Node(2);
         Node n3 = new Node(3);
         Node n4 = new Node(4);
+        Node n5 = new Node(5);
         n1.nexts.add(n2);
         n1.nexts.add(n3);
         n3.nexts.add(n2);
         n3.nexts.add(n4);
+        n2.nexts.add(n5);
         traverseBFS(n1);
+        System.out.println("------bfs");
+        traverseDFS(n1);
     }
 }
