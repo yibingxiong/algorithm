7e8ca574c68a85adab68768a8c870f2c890ff62c
易丙雄 <yibingxiong@126.com>
2020-11-10 10:29:05 +0800

    a

--- code/src/main/java/com/xiong/nowcoder/NC25.java
+++ code/src/main/java/com/xiong/nowcoder/NC25.java
@@ -1,4 +1,33 @@
 package com.xiong.nowcoder;
 
 public class NC25 {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+    }
+
+    public class Solution {
+        /**
+         * @param head ListNode类
+         * @return ListNode类
+         */
+        public ListNode deleteDuplicates(ListNode head) {
+            // write code here
+            if (head == null) {
+                return null;
+            }
+            ListNode pre = head;
+            ListNode cur = head.next;
+
+            while (cur != null) {
+                if (cur.val == pre.val) {
+                    pre.next = cur.next;
+                } else {
+                    pre = cur;
+                }
+                cur = cur.next;
+            }
+            return head;
+        }
+    }
 }
--- code/src/main/java/com/xiong/nowcoder/NC69.java
+++ code/src/main/java/com/xiong/nowcoder/NC69.java
@@ -1,4 +1,37 @@
 package com.xiong.nowcoder;
 
 public class NC69 {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public class Solution {
+        public ListNode FindKthToTail(ListNode head, int k) {
+            if (head == null) {
+                return null;
+            }
+            ListNode fast = head;
+            ListNode slow = head;
+
+            int i = 0;
+
+            while (fast != null && i < k) {
+                fast = fast.next;
+                i++;
+            }
+            if (fast == null && i < k) {
+                return null;
+            }
+            while (fast != null) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
 }
