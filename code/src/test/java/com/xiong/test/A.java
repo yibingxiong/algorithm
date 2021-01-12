f0526b3e3c27e5602b0081cf3863701b2b2ab3d1
yibingxiong <yibingxiong@126.com>
2021-01-13 00:09:09 +0800

    a

---
+++ code/src/test/java/com/xiong/test/A.java
@@ -0,0 +1,28 @@
+package com.xiong.test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by daxiong on 2020/11/21 11:13
+ */
+public class A {
+    private static final String a = "a";
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (Integer integer : arrayList) {
+
+        }
+        for (Integer integer : arrayList) {
+
+        }
+        /*for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            System.out.println(arg);
+        }*/
+
+        if (arrayList == null) {
+            System.out.println(3);
+        }
+    }
+}
