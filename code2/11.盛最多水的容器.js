60e146fc7367754dcb4555238151e15eb7c039a2
yibingxiong <yibingxiong@126.com>
2021-02-19 22:03:22 +0800

    11. 盛水最多的容器

--- code2/11.盛最多水的容器.js
+++ code2/11.盛最多水的容器.js
@@ -18,30 +18,30 @@
 /////第一遍//////
 
 // 方法1
-var maxArea = function (height) {
-  let i = 0;
-  let j = height.length - 1;
-  let max = 0;
-  while (i < j) {
-    let W = j - i;
-    let H = height[i] > height[j] ? height[j--] : height[i++];
-    max = Math.max(max, W*H);
-  }
-  return max;
-};
-
-// 方法2
 // var maxArea = function (height) {
+//   let i = 0;
+//   let j = height.length - 1;
 //   let max = 0;
-//   for (let i = 0; i < height.length; i++) {
-//     for (let j = i + 1; j < height.length; j++) {
-//       let area = (j - i) * Math.min(height[i], height[j]);
-//       if (area > max) {
-//         max = area;
-//       }
-//     }
+//   while (i < j) {
+//     let W = j - i;
+//     let H = height[i] > height[j] ? height[j--] : height[i++];
+//     max = Math.max(max, W*H);
 //   }
 //   return max;
 // };
+
+// 方法2
+var maxArea = function (height) {
+  let max = 0;
+  for (let i = 0; i < height.length; i++) {
+    for (let j = i + 1; j < height.length; j++) {
+      let area = (j - i) * Math.min(height[i], height[j]);
+      if (area > max) {
+        max = area;
+      }
+    }
+  }
+  return max;
+};
 // @lc code=end
 
