56b5adf3af6a2a0525abcf71bc2e1e61d2f37733
yibingxiong <yibingxiong@126.com>
2021-02-21 12:20:01 +0800

    66 2

--- code2/66.加一.js
+++ code2/66.加一.js
@@ -33,18 +33,33 @@
 // };
 
 // 方法2 很妙
+// var plusOne = function (digits) {
+//   for (let i = digits.length - 1; i >= 0; i--) {
+//     if (digits[i] === 9) {
+//       digits[i] = 0;
+//     } else {
+//       digits[i] += 1;
+//       return digits
+//     }
+//   }
+//   digits.unFshift(1)
+//   return digits;
+// };
+
+// 第2遍
 var plusOne = function (digits) {
   for (let i = digits.length - 1; i >= 0; i--) {
-    if (digits[i] === 9) {
-      digits[i] = 0;
-    } else {
+    if (digits[i] < 9) {
       digits[i] += 1;
-      return digits
+      return digits;
+    } else {
+      digits[i] = 0;
     }
   }
-  digits.unFshift(1)
+  if (digits[0] === 0) {
+    digits.unshift(1);
+  }
   return digits;
-
 };
 // @lc code=end
 
