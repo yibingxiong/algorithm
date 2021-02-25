27881bfa7dcfb20dd938520f20f892e51de1f9a1
yibingxiong <yibingxiong@126.com>
2021-02-25 08:45:17 +0800

    98

--- code2/98.验证二叉搜索树.js
+++ code2/98.验证二叉搜索树.js
@@ -13,37 +13,94 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
+// 第1遍
+
+// 1
+// /**
+//  * @param {TreeNode} root
+//  * @return {boolean}
+//  */
+// var isValidBST = function (root) {
+//     function isValid(root) {
+//         if (root === null) {
+//             return {
+//                 isValid: true,
+//                 max: Number.MIN_SAFE_INTEGER,
+//                 min: Number.MAX_SAFE_INTEGER,
+//             }
+//         }
+//         const l = isValid(root.left);
+//         const r = isValid(root.right);
+//         const min = Math.min(l.min, root.val, r.min);
+//         const max = Math.max(l.max, root.val, r.max);
+//         let v = l.isValid && r.isValid;
+//         if (root.left) {
+//             v = v && l.max < root.val;
+//         }
+//         if (root.right) {
+//             v = v && r.min > root.val;
+//         }
+//         return {
+//             isValid: v,
+//             max,
+//             min,
+//         }
+//     }
+//     return isValid(root).isValid;
+// };
+
+// 2
+// /**
+//  * @param {TreeNode} root
+//  * @return {boolean}
+//  */
+// var isValidBST = function (root) {
+//     if (root === null) {
+//         return true;
+//     }
+//     const stack = [];
+//     let last = Number.MIN_SAFE_INTEGER;
+//     while (stack.length || root) {
+//         while (root) {
+//             stack.push(root);
+//             root = root.left;
+//         }
+//         const l = stack.pop();
+//         if (l.val <= last) {
+//             return false;
+//         }
+//         last = l.val;
+//         root = l.right;
+//     }
+//     return true;
+// };
+
+// 3
 /**
  * @param {TreeNode} root
  * @return {boolean}
  */
 var isValidBST = function (root) {
+    if (root === null) {
+        return true;
+    }
+    let last = Number.MIN_SAFE_INTEGER;
+    let res = true;
     function isValid(root) {
         if (root === null) {
-            return {
-                isValid: true,
-                max: Number.MIN_SAFE_INTEGER,
-                min: Number.MAX_SAFE_INTEGER,
-            }
+            return;
         }
-        const l = isValid(root.left);
-        const r = isValid(root.right);
-        const min = Math.min(l.min, root.val, r.min);
-        const max = Math.max(l.max, root.val, r.max);
-        let v = l.isValid && r.isValid;
-        if (root.left) {
-            v = v && l.max < root.val;
+        isValid(root.left);
+        if (root.val <= last) {
+            res = false;
         }
-        if (root.right) {
-            v = v && r.min > root.val;
-        }
-        return {
-            isValid: v,
-            max,
-            min,
-        }
+        last = root.val;
+        isValid(root.right);
     }
-    return isValid(root).isValid;
+    isValid(root);
+
+    return res;
 };
 // @lc code=end
 
