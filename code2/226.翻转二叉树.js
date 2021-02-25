2e4c5bdc8f58824d3aceaa29fc8e5852b7ebf723
yibingxiong <yibingxiong@126.com>
2021-02-25 08:15:11 +0800

    226

--- code2/226.翻转二叉树.js
+++ code2/226.翻转二叉树.js
@@ -13,24 +13,57 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
+// 第1遍
+
+// 1
+// /**
+//  * @param {TreeNode} root
+//  * @return {TreeNode}
+//  */
+// var invertTree = function (root) {
+//     function invert(root) {
+//         if (root === null) {
+//             return null;
+//         }
+//         const left = invert(root.right);
+//         const right = invert(root.left);
+//         root.left = left;
+//         root.right = right;
+
+//         return root;
+//     }
+
+//     return invert(root);
+// };
+
+// 2
 /**
  * @param {TreeNode} root
  * @return {TreeNode}
  */
 var invertTree = function (root) {
-    function invert(root) {
-        if (root === null) {
-            return null;
-        }
-        const left = invert(root.right);
-        const right = invert(root.left);
-        root.left = left;
-        root.right = right;
-        
-        return root;
-    }
+  const queue = [];
+  if (root === null) {
+    return root;
+  }
 
-    return invert(root);
+  queue.push(root);
+
+  while (queue.length) {
+    const node = queue.shift();
+    const t = node.left;
+    node.left = node.right;
+    node.right = t;
+
+    if (node.left !== null) {
+      queue.push(node.left);
+    }
+    if (node.right !== null) {
+      queue.push(node.right);
+    }
+  }
+  return root;
 };
 // @lc code=end
 
