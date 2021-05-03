af71dcc4ba70f951bb7c3a1fc9471b285bc9a708
yibingxiong <yibingxiong@126.com>
2021-05-03 17:42:11 +0800

    77

--- code2/77.组合.js
+++ code2/77.组合.js
@@ -5,6 +5,30 @@
  */
 
 // @lc code=start
+// /**
+//  * @param {number} n
+//  * @param {number} k
+//  * @return {number[][]}
+//  */
+// var combine = function (n, k) {
+//   const res = [];
+//   function dfs(cur, temp) {
+//     if (temp.length === k) {
+//       res.push(temp.concat());
+//       return;
+//     }
+//     if (temp.length + (n - cur + 1) < k) {
+//       return;
+//     }
+//     dfs(cur + 1, temp);
+//     temp.push(cur);
+//     dfs(cur + 1, temp);
+//     temp.pop();
+//   }
+//   dfs(1, []);
+//   return res;
+// };
+
 /**
  * @param {number} n
  * @param {number} k
@@ -12,20 +36,20 @@
  */
 var combine = function (n, k) {
   const res = [];
-  function dfs(cur, temp) {
-    if (temp.length === k) {
-      res.push(temp.concat());
+  function dfs(tmp = [], i) {
+    if (tmp.length === k) {
+      res.push([...tmp]);
       return;
     }
-    if (temp.length + (n - cur + 1) < k) {
+    if (tmp.length + (n - i + 1) < k) {
       return;
     }
-    dfs(cur + 1, temp);
-    temp.push(cur);
-    dfs(cur + 1, temp);
-    temp.pop();
+    tmp.push(i);
+    dfs(tmp, i + 1);
+    tmp.pop();
+    dfs(tmp, i + 1);
   }
-  dfs(1, []);
+  dfs([], 1);
   return res;
 };
 // @lc code=end
